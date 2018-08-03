939cdad5853e72983cd6e7e035de6a23cde97887
cuma-literproject <cuma@literproject.com>
2018-08-03 14:27:13 +0900

    review redirect

--- app/containers/ReviewForm/reducer.js
+++ app/containers/ReviewForm/reducer.js
@@ -5,16 +5,34 @@
  */
 
 import { fromJS } from 'immutable';
-import { DEFAULT_ACTION, POST_ACTION } from './constants';
+import { DEFAULT_ACTION, POST_ACTION, POST_SEND, POST_SUCCESS, POST_ERROR } from './constants';
 
-export const initialState = fromJS({});
+export const initialState = fromJS({
+  loading: false,
+  error: false,
+  result: false,
+});
 
 function reviewFormReducer(state = initialState, action) {
   switch (action.type) {
     case DEFAULT_ACTION:
       return state;
-    case POST_ACTION:
+    case POST_ACTION:      
       return state;
+    case POST_SEND:
+      console.log("post_send");
+      return state
+            .set('loading', true)
+            .set('error', false);
+    case POST_SUCCESS:
+      return state
+            .set('loading', false)
+            .set('error', false)
+            .set('result', action.data);
+    case POST_ERROR:
+      return state
+            .set('loading', false)
+            .set('error', true);
     default:
       return state;
   }

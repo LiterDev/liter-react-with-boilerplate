63d10732bbb7fa277d5a4aa8eef1132e346f589b
neo <neo@literproject>
2018-08-07 09:46:39 +0900

    username getdata change in mypage global > userData > username

---
+++ app/components/TimeAt/index.js
@@ -0,0 +1,41 @@
+/**
+ *
+ * TimeAt
+ *
+ */
+
+import React from 'react';
+import moment from 'moment/min/moment-with-locales';
+import Moment from 'react-moment';
+
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+// import { FormattedMessage } from 'react-intl';
+// import messages from './messages';
+
+// Sets the moment instance to use.
+Moment.globalMoment = moment;
+
+// Set the locale for every react-moment instance to French.
+Moment.globalLocale = 'ko';
+
+// Set the output format for every react-moment instance.
+// Moment.globalFormat = 'D MMM YYYY';
+
+// Set the timezone for every instance.
+// Moment.globalTimezone = 'America/Los_Angeles';
+
+// Use a <span> tag for every react-moment instance.
+// Moment.globalElement = 'span';
+
+/* eslint-disable react/prefer-stateless-function */
+class TimeAt extends React.PureComponent {
+  render() {
+    return <Moment locale="ko" date={this.props.date} fromNow />;
+  }
+}
+
+TimeAt.propTypes = {};
+
+export default TimeAt;
---
+++ app/components/TimeAt/messages.js
@@ -0,0 +1,14 @@
+/*
+ * TimeAt Messages
+ *
+ * This contains all the text for the TimeAt component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.TimeAt.header',
+    defaultMessage: 'This is the TimeAt component !',
+  },
+});

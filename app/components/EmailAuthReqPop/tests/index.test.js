f8f7f1dc3bbc5f46bce870b001a6cdf8f689eb4c
neo <neo@literproject>
2018-08-09 09:07:18 +0900

    Email Auth Popup create

---
+++ app/components/EmailAuthReqPop/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import EmailAuthReqPop from '../index';
+
+describe('<EmailAuthReqPop />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});

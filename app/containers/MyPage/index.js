1db9b0e7e8a3bd38184169767b6ce3affd2ad8ce
neo <neo@literproject>
2018-08-13 17:53:03 +0900

    coin check

--- app/containers/MyPage/index.js
+++ app/containers/MyPage/index.js
@@ -290,7 +290,7 @@ export class MyPage extends React.PureComponent {
             </IconButton>
           </div>
           <div className={classes.row}>
-            {localStorage.getItem('hasWallet') ? (
+            {localStorage.getItem('hasWallet') === 'true' ? (
               <Typography variant="headline" className={classes.userCoin}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"

79a52ed06c0cd41586e5f2ac507e8bb947c94108
neo <neo@literproject>
2018-08-23 15:55:34 +0900

    share modify

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -105,7 +105,7 @@ function App(props) {
   return (
     <div className={classes.root}>
       <Helmet
-        titleTemplate="%s - 세상의 모든 리뷰, LITER"
+        titleTemplate="세상의 모든 리뷰, LITER - %s"
         defaultTitle="세상의 모든 리뷰, LITER"
       >
         {/* <meta name="description" content="A React.js Boilerplate application" />

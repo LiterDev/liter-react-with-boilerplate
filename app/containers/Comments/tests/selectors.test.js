b0b6e5d2ec2f9fa5f5593e9b3fac751a8eeea21b
neo <neo@literproject>
2018-08-27 18:30:54 +0900

    comment add

--- app/containers/Comments/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { selectCommentsDomain } from '../selectors';
-
-describe('selectCommentsDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});

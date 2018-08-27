7bf4d5856ddadf9de4d6c0edf01ded115092243a
colamoon <colamooon@gmail.com>
2018-08-27 15:32:42 +0900

    update follow text

--- app/components/FollowAjxButton/index.js
+++ app/components/FollowAjxButton/index.js
@@ -50,7 +50,6 @@ const styles = theme => ({
 
 /* eslint-disable react/prefer-stateless-function */
 class FollowAjxButton extends React.PureComponent {
-
   constructor(props) {
     super(props);
 
@@ -58,7 +57,7 @@ class FollowAjxButton extends React.PureComponent {
       followStatus: false,
       followId: null,
       myFollowYn: 0,
-    }
+    };
 
     this.requestAjx = this.requestAjx.bind(this);
     this.handleSetFollow = this.handleSetFollow.bind(this);
@@ -83,64 +82,61 @@ class FollowAjxButton extends React.PureComponent {
     // this.request(requestURL, data, options);
   }
 
-
-
   requestAjx = (method, sendType, requestURL, data, options) => {
     const self = this;
     axios({
-      'method': sendType,
-      'headers': options,
-      'url': requestURL,
-      'data': data,
+      method: sendType,
+      headers: options,
+      url: requestURL,
+      data: data,
     })
-    .then(function (response) {
-      if(method == 'setFollow') {
-        self.setState({
-          'followStatus': 1,
-          'myFollowYn': 1,
-        });
-      } else if(method == 'setUnFollow') {
-        self.setState({
-          'followStatus': false,
-          'myFollowYn': false,
-        });
-      }
-      return response;
-    })
-    .catch(function (error) {
-      console.log(error);
-      return error;
-    });
-
-  }
+      .then(function(response) {
+        if (method == 'setFollow') {
+          self.setState({
+            followStatus: 1,
+            myFollowYn: 1,
+          });
+        } else if (method == 'setUnFollow') {
+          self.setState({
+            followStatus: false,
+            myFollowYn: false,
+          });
+        }
+        return response;
+      })
+      .catch(function(error) {
+        console.log(error);
+        return error;
+      });
+  };
 
   handleSetFollow(followId) {
-    console.log("Set");
+    console.log('Set');
     const requestURL = `${process.env.API_URL}/follow`;
     const accessToken = localStorage.getItem('accessToken');
     const token = `Bearer ${accessToken}`;
     const options = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Authorization': token,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      Authorization: token,
     };
     const data = JSON.stringify({
-      'followId': followId
+      followId: followId,
     });
     this.requestAjx('setFollow', 'POST', requestURL, data, options);
   }
 
   handleSetUnFollow(follodId) {
-    console.log("UnSet");
+    console.log('UnSet');
     const requestURL = `${process.env.API_URL}/follow/${follodId}`;
     const accessToken = localStorage.getItem('accessToken');
     const token = `Bearer ${accessToken}`;
     const options = {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Authorization': token,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      Authorization: token,
     };
     const data = {};
     this.requestAjx('setUnFollow', 'DELETE', requestURL, data, options);
@@ -157,38 +153,50 @@ class FollowAjxButton extends React.PureComponent {
     const { classes } = this.props;
     const { followEmail, followId, followYn } = this.props;
     const { followStatus, myFollowYn } = this.state;
-    
-    const userEmail = localStorage.getItem('username');
-    const bSignIn = (userEmail)?true:false;
 
-    if(!followStatus) {
-      if(myFollowYn > 0) {
+    const userEmail = localStorage.getItem('username');
+    const bSignIn = userEmail ? true : false;
+
+    if (!followStatus) {
+      if (myFollowYn > 0) {
         return (
           <div className={classes.followBox}>
-            <Typography className={classes.unButtonText} onClick={() => { this.handleSetUnFollow(followId) }} >
+            <Typography
+              className={classes.unButtonText}
+              onClick={() => {
+                this.handleSetUnFollow(followId);
+              }}
+            >
               <FormattedMessage {...messages.unButtonTitle} />
             </Typography>
           </div>
         );
       }
-      if(bSignIn && userEmail == followEmail) {
-        return (
-          <div className={classes.followBox}>
-          </div>
-        );
+      if (bSignIn && userEmail == followEmail) {
+        return <div className={classes.followBox} />;
       }
       return (
         <div className={classes.followBox}>
-          <Typography className={classes.buttonText} onClick={() => { this.handleSetFollow(followId) }} >
+          <Typography
+            className={classes.buttonText}
+            onClick={() => {
+              this.handleSetFollow(followId);
+            }}
+          >
             <FormattedMessage {...messages.buttonTitle} />
           </Typography>
         </div>
       );
     } else {
-      if(followStatus > 0) {
+      if (followStatus > 0) {
         return (
           <div className={classes.followBox}>
-            <Typography className={classes.unButtonText} onClick={() => { this.handleSetUnFollow(followId) }} >
+            <Typography
+              className={classes.unButtonText}
+              onClick={() => {
+                this.handleSetUnFollow(followId);
+              }}
+            >
               <FormattedMessage {...messages.unButtonTitle} />
             </Typography>
           </div>
@@ -196,13 +204,18 @@ class FollowAjxButton extends React.PureComponent {
       }
       return (
         <div className={classes.followBox}>
-          <Typography className={classes.buttonText} onClick={() => { this.handleSetFollow(followId) }} >
+          <Typography
+            className={classes.buttonText}
+            onClick={() => {
+              this.handleSetFollow(followId);
+            }}
+          >
             <FormattedMessage {...messages.buttonTitle} />
           </Typography>
         </div>
       );
     }
-    return <div />
+    return <div />;
   }
 }
 
